238a6174d2dd52567965970183861f4993c9b9f5
OnePointChetan <chetan.hatnapure@onepointltd.com>
2024-06-04 20:08:16 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -24,7 +24,7 @@ my_dataframe = session.table("smoothies.public.fruit_options").select(col('FRUIT
 #st.dataframe(data=my_dataframe, use_container_width=True)
 #st.stop()
 
-pd_df = my_dataframe.to_pandas()
+# pd_df = my_dataframe.to_pandas()
 #st.dataframe(pd_df)
 #st.stop()
 
@@ -43,9 +43,9 @@ if ingredients_list:
         search_on=pd_df.loc[pd_df['FRUIT_NAME'] == fruit_chosen, 'SEARCH_ON'].iloc[0]
         st.write('The search value for ', fruit_chosen,' is ', search_on, '.')
         
-        # st.subheader(fruit_chosen + ' Nutrition Information') 
-        st.subheader('The Search Value for ' + fruit_chosen + ' is ' + search_on) 
-        fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
+        st.subheader(fruit_chosen + ' Nutrition Information') 
+        # st.subheader('The Search Value for ' + fruit_chosen + ' is ' + search_on) 
+        fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + fruit_chosen)
         fv_df = st.dataframe(data=fruityvice_response.json(), use_container_width=True)
     #st.write(ingredients_string)
 
